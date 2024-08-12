8fd71544436df43fc768ce53b502096675610582
Andre855-bit <andremachado855@gmail.com>
2024-08-12 19:57:31 -0300

    quarto exercício

---
+++ quarto.php
@@ -0,0 +1,21 @@
+<?php 
+$data = "12 de agosto de 2024";
+$salário = 850.00;
+$idade = 18;
+$cargo = 'Estagiário';
+$resultado = true;
+
+
+echo "Data: $data <br />";
+echo "Salário: $salário <br />";
+echo "Idade: $idade <br />";
+echo "Cargo: $cargo <br />";
+
+if ($resultado) {
+echo "Resultado é verdadeiro.";
+}
+
+
+
+
+ ?>
