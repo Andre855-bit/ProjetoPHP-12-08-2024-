dd7889bc0ef327208a306dd4cc0bd36451f0da9d
Andre855-bit <andremachado855@gmail.com>
2024-10-16 22:04:06 -0300

    bacalhão

---
+++ request.php
@@ -0,0 +1,21 @@
+<?php 
+
+$login =$_REQUEST['login'];
+$senha = $_REQUEST['senha'];
+
+if ($login == 'etecia'&& $senha=='etecia') {
+	echo "Bem vindo ao sistema";
+	}else {
+		echo "Erro ao acessar o sistema";
+	}
+
+
+
+
+
+
+
+
+
+
+ ?>
